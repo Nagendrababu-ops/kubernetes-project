ced12ad18da931b38694a7db632d0cb3dfb4fa30
Nagendrababu <d.nagendrababu@ripplestreet.com>
2025-06-24 14:36:56 +0530

    code changes

--- microservices/user-service/src/main/java/com/bookstore/userservice/JwtAuthenticationFilter.java
+++ microservices/user-service/src/main/java/com/bookstore/userservice/JwtAuthenticationFilter.java
@@ -1,13 +1,12 @@
 package com.bookstore.userservice;
 
+import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.bookstore.userservice.JwtTokenProvider;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
