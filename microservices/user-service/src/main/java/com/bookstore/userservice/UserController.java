21fee0e88c706ff2c86d427a40c5c771c9c8acdf
Nagendrababu-ops <nagendrababu9527@gmail.com>
2025-07-04 12:39:03 +0530

    Update UserController.java

--- microservices/user-service/src/main/java/com/bookstore/userservice/UserController.java
+++ microservices/user-service/src/main/java/com/bookstore/userservice/UserController.java
@@ -1,49 +1,41 @@
 package com.bookstore.userservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.bookstore.userservice.dto.LoginRequest;
+import com.bookstore.userservice.model.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final JwtTokenProvider jwtProvider;
+    private final AuthenticationManager authManager;
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    public UserController(UserService us, JwtTokenProvider jp, AuthenticationManager am) {
+        this.userService = us;
+        this.jwtProvider = jp;
+        this.authManager = am;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
-        Optional<User> existingUser = userService.findByUsername(user.getUsername());
-        if (existingUser.isPresent()) {
-            return ResponseEntity.badRequest().body("Username already taken");
+        if (userService.findByUsername(user.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already exists");
         }
-
-        User registeredUser = userService.register(user);
-        return ResponseEntity.ok(registeredUser);
+        return ResponseEntity.ok(userService.register(user));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                )
-        );
-
-        // Generate JWT token
-        String token = jwtTokenProvider.createToken(loginRequest.getUsername());
+    public ResponseEntity<?> login(@RequestBody LoginRequest req) {
+        authManager.authenticate(
+            new UsernamePasswordAuthenticationToken(req.getUsername(), req.getPassword()));
+        String token = jwtProvider.createToken(req.getUsername());
         return ResponseEntity.ok(Map.of("token", token));
     }
 }
