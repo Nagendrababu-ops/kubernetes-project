acd696cb5de3ea480513bc52953c64efdf9a6085
Nagendrababu-ops <nagendrababu9527@gmail.com>
2025-07-04 12:46:46 +0530

    Update JwtAuthenticationFilter.java

--- microservices/user-service/src/main/java/com/bookstore/userservice/JwtAuthenticationFilter.java
+++ microservices/user-service/src/main/java/com/bookstore/userservice/JwtAuthenticationFilter.java
@@ -10,7 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
