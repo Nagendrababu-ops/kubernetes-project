6477e0822bd0cb038d2d3bc60aec2d6b97fc5825
Nagendrababu-ops <nagendrababu9527@gmail.com>
2025-07-04 12:24:49 +0530

    Update UserRepository.java

--- microservices/user-service/src/main/java/com/bookstore/userservice/UserRepository.java
+++ microservices/user-service/src/main/java/com/bookstore/userservice/UserRepository.java
@@ -1,9 +1,9 @@
-package com.bookstore.userservice;
+package com.bookstore.userservice.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bookstore.userservice.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
-
